ccb7bf34d30deba098346f9ab443eb727f929a64
ludovicobesana <ludovicobesanaguide@gmail.com>
2023-03-22 03:02:39 +0100

    feat: Add initial files

---
+++ docusaurus.config.js
@@ -0,0 +1,129 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Javscript from scratch',
+  tagline: 'SheTech Bootcamp',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Javascript from scratch',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Bootcamp',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Entra in community',
+                href: 'https://shetechitaly.org/unisciti-a-noi/',
+              },
+              {
+                label: 'Instagram',
+                href: 'https://www.instagram.com/shetech_italy/',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/shetech/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/shetechitaly',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} SheTech. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Fai networking',
+    Svg: require('@site/static/img/undraw_having_fun_re_vj4h.svg').default,
+    description: (
+      <>
+       Approfitta di questa opportunità per fare networking con i tuoi compagni di corso e con i mentor e mentee. 
+       Il networking può portare a nuove opportunità lavorative, partnership e amicizie durature.
+      </>
+    ),
+  },
+  {
+    title: 'Contribuisci alla community',
+    Svg: require('@site/static/img/undraw_web_development_0l6v.svg').default,
+    description: (
+      <>
+        Se dovessi finire prima gli esercizi o ti senti particolarmente sicur* di un determinato argomento, 
+        considera l'opportunità di aiutare i mentor e le mentee a spiegare i concetti al resto del tuo gruppo.
+      </>
+    ),
+  },
+  {
+    title: 'Sfrutta il supporto',
+    Svg: require('@site/static/img/undraw_people_re_8spw.svg').default,
+    description: (
+      <>
+        Durante tutto il percorso del bootcamp, sarai seguit* da mentor esperti che saranno pronti ad aiutarti in caso di difficoltà. 
+        Approfitta del loro supporto per superare eventuali ostacoli e per imparare il più possibile.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
